c7b72fe5ee609d6beac74bdee4edbd79ec3be9e9
Jakub Kadlcik <frostyx@email.cz>
2021-04-28 01:28:08 +0200

    Change model data type to be a record

--- src/Main.elm
+++ src/Main.elm
@@ -37,27 +37,29 @@ type alias Option =
 
 
 type alias Model =
-    List Option
+    { options : List Option }
 
 
 init : Model
 init =
-    [ Option "set number" "(add-hook 'prog-mode-hook #'display-line-numbers-mode)" Nothing
-    , Option "set nofoldenable" "TODO" Nothing
-    , Option "set autowrite" "TODO" Nothing
-    , Option "set showmatch" "TODO" Nothing
-    , Option "set tabstop" "TODO" (Just "4")
-    , Option "set shiftwidth" "TODO" (Just "4")
-    , Option "set softtabstop" "TODO" (Just "4")
-    , Option "set autoindent" "TODO" Nothing
-    , Option "set smartindent" "TODO" Nothing
-    , Option "set scrolloff" "TODO" (Just "5")
-    , Option "set pastetoggle" "TODO NOOP" (Just "<F2>")
-    , Option "nmap <silent> <c-k> :wincmd k<CR>" "TODO" Nothing
-    , Option "nmap <silent> <c-j> :wincmd j<CR>" "TODO" Nothing
-    , Option "nmap <silent> <c-h> :wincmd h<CR>" "TODO" Nothing
-    , Option "nmap <silent> <c-l> :wincmd l<CR>" "TODO" Nothing
-    ]
+    { options =
+        [ Option "set number" "(add-hook 'prog-mode-hook #'display-line-numbers-mode)" Nothing
+        , Option "set nofoldenable" "TODO" Nothing
+        , Option "set autowrite" "TODO" Nothing
+        , Option "set showmatch" "TODO" Nothing
+        , Option "set tabstop" "TODO" (Just "4")
+        , Option "set shiftwidth" "TODO" (Just "4")
+        , Option "set softtabstop" "TODO" (Just "4")
+        , Option "set autoindent" "TODO" Nothing
+        , Option "set smartindent" "TODO" Nothing
+        , Option "set scrolloff" "TODO" (Just "5")
+        , Option "set pastetoggle" "TODO NOOP" (Just "<F2>")
+        , Option "nmap <silent> <c-k> :wincmd k<CR>" "TODO" Nothing
+        , Option "nmap <silent> <c-j> :wincmd j<CR>" "TODO" Nothing
+        , Option "nmap <silent> <c-h> :wincmd h<CR>" "TODO" Nothing
+        , Option "nmap <silent> <c-l> :wincmd l<CR>" "TODO" Nothing
+        ]
+    }
 
 
 type Msg
@@ -90,7 +92,7 @@ viewOptionSections : Model -> Html Msg
 viewOptionSections model =
     div []
         ([ h2 [] [ text "Vim options" ] ]
-            ++ List.map viewOption model
+            ++ List.map viewOption model.options
         )
 
 
@@ -167,7 +169,7 @@ viewTable model =
                     , th [] [ text "Emacs" ]
                     ]
               ]
-            , List.map viewOptionRow model
+            , List.map viewOptionRow model.options
             ]
         )
 
