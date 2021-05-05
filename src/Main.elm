056983d7c8ea928b4f54eefb2fb393b318b8773d
Jakub Kadlcik <frostyx@email.cz>
2021-05-05 21:53:10 +0200

    When vimConfig is empty print default emacsConfig

--- src/Main.elm
+++ src/Main.elm
@@ -25,45 +25,53 @@ subscriptions model =
     Accordion.subscriptions model.accordionState AccordionMsg
 
 
+initOptions : Array.Array Option
+initOptions =
+    Array.fromList
+        [ Option "set number" "(add-hook 'prog-mode-hook #'display-line-numbers-mode)" Nothing Compatible
+        , Option "set nocompatible" "TODO" Nothing NOOP
+        , Option "set nofoldenable" "TODO" Nothing Unknown
+        , Option "set autowrite" "TODO" Nothing Unknown
+        , Option "set showmatch" "TODO" Nothing Unknown
+        , Option "set tabstop" "TODO" (Just "4") Unknown
+        , Option "set shiftwidth" "(setq evil-shift-width {0})" (Just "4") Unknown
+        , Option "set softtabstop" "TODO" (Just "4") Unknown
+        , Option "set autoindent" "TODO" Nothing Unknown
+        , Option "set smartindent" "TODO" Nothing Unknown
+        , Option "set scrolloff" "TODO" (Just "5") Unknown
+        , Option "set pastetoggle" "TODO NOOP" (Just "<F2>") Unknown
+        , Option "nmap <silent> <c-k> :wincmd k<CR>" "TODO" Nothing Unknown
+        , Option "nmap <silent> <c-j> :wincmd j<CR>" "TODO" Nothing Unknown
+        , Option "nmap <silent> <c-h> :wincmd h<CR>" "TODO" Nothing Unknown
+        , Option "nmap <silent> <c-l> :wincmd l<CR>" "TODO" Nothing Unknown
+        ]
+
+
+initEmacsConfig : String
+initEmacsConfig =
+    ";; Put this into init.el\n"
+        ++ "(use-package evil\n"
+        ++ "  :ensure t\n"
+        ++ "  :init\n"
+        ++ "  (setq evil-search-module 'evil-search)\n"
+        ++ "  (setq evil-ex-complete-emacs-commands nil)\n"
+        ++ "  (setq evil-vsplit-window-right t)\n"
+        ++ "  (setq evil-split-window-below t)\n"
+        ++ "  (setq evil-shift-round nil)\n"
+        ++ "  (setq evil-want-C-u-scroll t)\n"
+        ++ "  (setq evil-ex-set-initial-state 'normal)\n"
+        ++ "  :config\n"
+        ++ "  (evil-mode))\n"
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { options =
-            Array.fromList
-                [ Option "set number" "(add-hook 'prog-mode-hook #'display-line-numbers-mode)" Nothing Compatible
-                , Option "set nocompatible" "TODO" Nothing NOOP
-                , Option "set nofoldenable" "TODO" Nothing Unknown
-                , Option "set autowrite" "TODO" Nothing Unknown
-                , Option "set showmatch" "TODO" Nothing Unknown
-                , Option "set tabstop" "TODO" (Just "4") Unknown
-                , Option "set shiftwidth" "(setq evil-shift-width {0})" (Just "4") Unknown
-                , Option "set softtabstop" "TODO" (Just "4") Unknown
-                , Option "set autoindent" "TODO" Nothing Unknown
-                , Option "set smartindent" "TODO" Nothing Unknown
-                , Option "set scrolloff" "TODO" (Just "5") Unknown
-                , Option "set pastetoggle" "TODO NOOP" (Just "<F2>") Unknown
-                , Option "nmap <silent> <c-k> :wincmd k<CR>" "TODO" Nothing Unknown
-                , Option "nmap <silent> <c-j> :wincmd j<CR>" "TODO" Nothing Unknown
-                , Option "nmap <silent> <c-h> :wincmd h<CR>" "TODO" Nothing Unknown
-                , Option "nmap <silent> <c-l> :wincmd l<CR>" "TODO" Nothing Unknown
-                ]
+    ( { options = initOptions
 
       -- Use `Accordion.initialState` to have everything collapsed
       , accordionState = Accordion.initialStateCardOpen "set nofoldenable"
       , vimConfig = "Foo"
-      , emacsConfig =
-            ";; Put this into init.el\n"
-                ++ "(use-package evil\n"
-                ++ "  :ensure t\n"
-                ++ "  :init\n"
-                ++ "  (setq evil-search-module 'evil-search)\n"
-                ++ "  (setq evil-ex-complete-emacs-commands nil)\n"
-                ++ "  (setq evil-vsplit-window-right t)\n"
-                ++ "  (setq evil-split-window-below t)\n"
-                ++ "  (setq evil-shift-round nil)\n"
-                ++ "  (setq evil-want-C-u-scroll t)\n"
-                ++ "  (setq evil-ex-set-initial-state 'normal)\n"
-                ++ "  :config\n"
-                ++ "  (evil-mode))\n"
+      , emacsConfig = initEmacsConfig
       }
     , Cmd.none
     )
@@ -78,7 +86,11 @@ update msg model =
             )
 
         Convert vimConfig ->
-            ( { model | emacsConfig = convertVimToEmacs vimConfig model.options }, Cmd.none )
+            if vimConfig |> String.isEmpty then
+                ( { model | emacsConfig = initEmacsConfig }, Cmd.none )
+
+            else
+                ( { model | emacsConfig = convertVimToEmacs vimConfig model.options }, Cmd.none )
 
         SetOptionValue value ->
             let
