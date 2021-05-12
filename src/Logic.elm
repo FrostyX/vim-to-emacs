96bbb4bec020b031798a14f602f0a56726abdb08
Jakub Kadlcik <frostyx@email.cz>
2021-05-12 23:20:12 +0200

    Show real vim documentation links

--- src/Logic.elm
+++ src/Logic.elm
@@ -6,4 +6,10 @@ import String.Interpolate exposing (interpolate)
 
 vimDocumentation : Option -> String
 vimDocumentation option =
-    interpolate "https://vimhelp.org/options.txt.html#%27number%27" []
+    interpolate "https://vimhelp.org/options.txt.html#%27{0}%27" [ vimName option ]
+
+
+vimName : Option -> String
+vimName option =
+    -- We need to drop the `set' part
+    option.vim |> String.dropLeft 4
